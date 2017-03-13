68a8629d72fb494bcd8075bbb2e2a3f0b626cefc
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-03-13 10:58:28 +0100

    Added App component, moment-timezone library, hard-coded a few days

---
+++ VueJS 2/Projekty/Calendar/src/components/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="" v-for="day in days">
+      {{ day }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello World',
+      month: 3,
+      year: 2017
+    }
+  },
+  computed: {
+    days() {
+      let days = [];
+      let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
+      do {
+        days.push(currentDay);
+        // this.$moment because it needs to be refreshed every time
+        currentDay = this.$moment(currentDay).add(1, 'days')
+      } while ((currentDay.month() + 1) === this.month);
+      return days
+    }
+  },
+}
+</script>
