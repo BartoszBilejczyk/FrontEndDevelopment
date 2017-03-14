34210f7293529522a49a60d8e932a9a9eaf50c34
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-03-14 11:11:10 +0100

    Moved movie-list rendering to new component

--- VueJS 2/Projekty/MovieDB/movieDB/src/components/MovieList.vue
+++ VueJS 2/Projekty/MovieDB/movieDB/src/components/MovieList.vue
@@ -1,10 +1,58 @@
 <template lang="html">
-
+  <div class="movie-category-wrapper">
+    <div class="movie" v-for="(popularMovie, index) in movieData.popular.results">
+      <div class="movie-poster"><img :src="config.images.secure_base_url + config.images.poster_sizes[3] + movieData.popular.results[index].poster_path" alt=""></div>
+      <p class="movie-title">{{ popularMovie.title }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  
+  import axios from 'axios'
+  import stored from '../stored.js'
+
+  export default {
+    data () {
+      return {
+        stored,
+        movieData: {
+          popular: {}
+        },
+        config: []
+      }
+    },
+    methods: {
+      fetchConfig () {
+        var self = this
+        axios.get(`https://api.themoviedb.org/3/configuration?api_key=${stored.apiKey}`)
+          .then(function (response) {
+            self.config = response.data
+            console.log(self.config)
+          })
+      },
+      fetchPopular () {
+        var self = this
+        axios.get(`https://api.themoviedb.org/3/movie/popular?&api_key=${stored.apiKey}`)
+          .then(function (response) {
+            let data = response.data
+            self.movieData.popular = data
+            self.movieData.popular.results = self.movieData.popular.results.slice(0, 5)
+            console.log(self.movieData.popular.results.slice(0, 5))
+          })
+      }
+
+      // for (var i = 0; i < stored.listTypes.length - 1; i++) {
+      //   axios.get(`https://api.themoviedb.org/3/movie/${stored.listTypes[i].query}?api_key=${stored.apiKey}`)
+      //     .then(function (response) {
+      //       let data = response.data
+      //       console.log(data)
+      //     })
+      // }
+    },
+    created () {
+      this.fetchConfig()
+      this.fetchPopular()
+    }
 }
 </script>
 
