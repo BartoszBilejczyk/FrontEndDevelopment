ae829e13f6e026de65345a5f1cc049d80b444112
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-03-22 17:35:26 +0100

    Imported files from /style to App.vue

--- VueJS 2/Projekty/10Clouds/src/App.vue
+++ VueJS 2/Projekty/10Clouds/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    
+    Hello
   </div>
 </template>
 
@@ -11,7 +11,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
+@import './style/globals.scss';
+@import './style/responsive.scss';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +24,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
 </style>
