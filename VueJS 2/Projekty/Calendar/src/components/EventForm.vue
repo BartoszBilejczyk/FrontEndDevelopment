f9fdfe6628359c065ad12cec9ef143352be67b1c
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-03-29 18:54:44 +0200

    Added automated focus for new event, accepting events with 'enter'

--- VueJS 2/Projekty/Calendar/src/components/EventForm.vue
+++ VueJS 2/Projekty/Calendar/src/components/EventForm.vue
@@ -1,8 +1,9 @@
 <template lang="html">
 <div id="event-form" :class="{ active: active }" :style="{top: top, left: left}">
     <h4>Add event</h4>
+    <span v-if="error" class="error">Please provide a name for the event</span>
     <div class="text">
-      <input type="text" v-model="description" :placeholder="inputPlaceholder">
+      <input type="text" v-focus v-model="description" :placeholder="inputPlaceholder" @keyup.enter="create">
     </div>
     <div class="buttons">
       <button @click="create">Create</button>
@@ -15,17 +16,23 @@
 export default {
   data() {
     return {
-      description: ''
+      description: '',
+      error: false
     }
   },
   methods: {
     close() {
       this.$store.commit('eventFormActive', false)
-      console.log(this.$moment().format('DD MM YYYY'))
     },
     create() {
-      this.$store.commit('addEvent', this.description)
-      this.close()
+      if (this.description.length > 0) {
+        this.error = false
+        this.$store.commit('addEvent', this.description)
+        this.description = ''
+        this.close()
+      } else {
+        this.error = true
+      }
     }
   },
   computed: {
@@ -44,6 +51,13 @@ export default {
     },
     left() {
       return `${this.$store.state.eventFormPositionX}px`
+    }
+  },
+  directives: {
+    focus: {
+      update(el) {
+        el.focus()
+      }
     }
   }
 }
