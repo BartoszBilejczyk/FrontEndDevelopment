f22ef9e1b5a558196a7150c1c977735c7a32e34d
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-03-29 10:15:11 +0200

    Form displays specific placeholder if the day is today

--- VueJS 2/Projekty/Calendar/src/components/EventForm.vue
+++ VueJS 2/Projekty/Calendar/src/components/EventForm.vue
@@ -2,7 +2,7 @@
 <div id="event-form" :class="{ active: active }" :style="{top: top, left: left}">
     <h4>Add event</h4>
     <div class="text">
-      <input type="text" v-model="description">
+      <input type="text" v-model="description" :placeholder="inputPlaceholder">
     </div>
     <div class="buttons">
       <button @click="create">Create</button>
@@ -21,6 +21,7 @@ export default {
   methods: {
     close() {
       this.$store.commit('eventFormActive', false)
+      console.log(this.$moment().format('DD MM YYYY'))
     },
     create() {
       this.$store.commit('addEvent', this.description)
@@ -28,6 +29,13 @@ export default {
     }
   },
   computed: {
+    inputPlaceholder() {
+      if (this.$store.state.eventFormDay.format('DD MM YYYY') == this.$moment().format('DD MM YYYY')) {
+        return 'What are your plans for today?'
+      } else {
+        return 'What are your plans?'
+      }
+    },
     active() {
       return this.$store.state.eventFormActive
     },
