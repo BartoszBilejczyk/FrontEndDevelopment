8d09e8b781ab0ab279bf134e1217bcfd97a9984b
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-03-30 15:00:51 +0200

    Loading initial data into state when loading, changes saved on server

--- VueJS 2/Projekty/Calendar/server.js
+++ VueJS 2/Projekty/Calendar/server.js
@@ -5,24 +5,33 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
+const moment = require('moment-timezone');
+moment.tz.setDefault('UTC');
+const serialize = require('serialize-javascript');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// these are on the server now
+let events = [
+  { description: 'Do laundry', date: moment('2017-03-03', 'YYYY-MM-DD') },
+  { description: 'Meeting with CEO', date: moment('2017-04-03', 'YYYY-MM-DD') },
+  { description: `Standup in Jack's Cinema`, date: moment('2017-02-03', 'YYYY-MM-DD') }
+]
+
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
-  res.send(template);
+  let contentMarker = '<!--APP-->';
+  // it is parsed to index.html (serialized) and available
+  res.send(template.replace(contentMarker, `<script>var __INITIAL_STATE__ = ${serialize(events)}</script>`));
 
 });
-
-let events = [];
-
 
 // this is used to pass the event as json object
 app.use(require('body-parser').json());
 
 // first argument is path, second is where we handle our event
 app.post('/add_event', (req, res) => {
-  //to be able to send data back to the browser  
+  //to be able to send data back to the browser
   events.push(req.body)
   res.sendStatus(200)
 });
--- VueJS 2/Projekty/Calendar/src/web.entry.js
+++ VueJS 2/Projekty/Calendar/src/web.entry.js
@@ -2,22 +2,25 @@ import Vue from 'vue';
 import './style.scss';
 
 import store from './store'
-import App from './components/App.vue';
 
 import moment from '../node_modules/moment-timezone'
 moment.tz.setDefault('UTC')
 Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
 
-//replace state object from index.js with object I provide here
-let events = [
-  { description: 'Do laundry', date: moment('2017-03-03', 'YYYY-MM-DD') },
-  { description: 'Meeting with CEO', date: moment('2017-04-03', 'YYYY-MM-DD') },
-  { description: `Standup in Jack's Cinema`, date: moment('2017-02-03', 'YYYY-MM-DD') }
-]
+import App from './components/App.vue';
 
+
+let events = window.__INITIAL_STATE__.map(event => {
+  return {
+    description: event.description,
+    date: moment(event.date)
+  }
+})
+
+//replace state object from index.js with object I provide here
 // initialState merges store.state and then events is passed into it, and upon loading store.state.events is not empty anymore
 
-let initialState = Object.assign({, store.state, { events }})
+let initialState = Object.assign({}, store.state, { events })
 store.replaceState(initialState)
 
 new Vue({
