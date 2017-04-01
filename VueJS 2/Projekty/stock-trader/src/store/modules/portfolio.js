99d9e757c2b406398bc4960006af919531f3548f
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-04-01 16:39:54 +0200

    Added portfolio module (vuex)

---
+++ VueJS 2/Projekty/stock-trader/src/store/modules/portfolio.js
@@ -0,0 +1,64 @@
+const state = {
+  funds: 10000,
+  // our portfolio, not all stocks available
+  stocks: []
+}
+
+const mutations = {
+  // three properties come from stock.vue. I will get the const order
+  'BUY_STOCK' (state, {stockId, quantity, stockPrice}) {
+    // check if I have this stock already. This will return true if stockId added is the same as stockId that is already in portfolio
+    const record = state.stocks.find(element => element.id === stockId)
+    if (record) {
+      // I have the stock, don't push new
+      record.quantity += quantity
+    } else {
+      state.stocks.push({
+        id: stockId,
+        quantity: quantity
+      })
+    },
+    state.funds -= stockPrice * quantity
+  },
+  'SELL_STOCK' (state, {stockId, quantity, stockPrice}) {
+    const record = state.stocks.find(element => element.id === stockId)
+    // possible to sell only if I have more than I want to sell
+    if (record.quantity > quantity) {
+      record.quantity -= quantity;
+    } else {
+      state.stocks.splice(state.stocks.indexOf(record), 1)
+    }
+    state.funds += stockPrice * quantity
+  }
+}
+
+const actions {
+  sellStock ({commit}, order) {
+    commit('SELL_STOCK', order)
+  }
+}
+
+const getters = {
+  // I wanna be able to get my stock portfolio
+  stockPortfolio (state, getters) {
+    return state.stocks.map(stock => {
+      const record = getters.stocks.find(element => element.id == stock.id)
+      return {
+        id: stock.id,
+        quantity: stock.quantity
+        name: record.name,
+        price: record.price
+      }
+    })
+  }
+  funds (state) {
+    return state.funds
+  }
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
