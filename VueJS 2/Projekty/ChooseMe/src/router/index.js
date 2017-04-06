1d5c72511565750f5309d564e9f616dd795c49f2
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2017-04-06 21:22:38 +0200

    deleted unused paths from router

--- VueJS 2/Projekty/ChooseMe/src/router/index.js
+++ VueJS 2/Projekty/ChooseMe/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home.vue'
-import DrinksDesserts from '../components/drinks/DrinksDesserts.vue'
-import MainCourses from '../components/main/MainCourses.vue'
 import Soups from '../components/soup/Soups.vue'
 
 Vue.use(Router)
